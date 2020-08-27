9db8f9fa129a43eab592391ce0e5f815cc143a75
shubham <chd.nonu26@gmail.com>
2020-08-27 12:28:50 +0530

    runn

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const helper=require('./Routes/addUsers')
 var io=socket(server)
 global["XMLHttpRequest"] = require("xmlhttprequest").XMLHttpRequest
 
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 var socket_id;
 const rooms=[]
 var session_destroy=(chunk)=>{
